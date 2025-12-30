dac8ce50571cf73ebb952e499fc2ccf728720f8e
coetz3r <coetz3r@gmail.com>
2025-12-30 15:21:55 +0200

    Fix script to run Python GUI with python3

--- run_agent_gui.sh
+++ run_agent_gui.sh
@@ -1,12 +1,3 @@
-
 #!/bin/bash
-# Go to the folder where the script lives
 cd "$(dirname "$0")"
-# Run the Python GUI
-#python3 agent_gui.py
-
-
-
-
-#cd "$(dirname "$0")"
-./agent_gui.py
+python3 agent_gui.py
